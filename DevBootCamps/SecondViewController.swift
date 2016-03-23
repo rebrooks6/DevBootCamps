88929ba3331a5a1a798ce6cbac6c2146882f03cc
Kayla Brooks <BrooksFamily@Kaylas-MacBook-Pro-3.local>
2016-03-23 11:57:15 -0500

    Initial Commit

---
+++ DevBootCamps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DevBootCamps
+//
+//  Created by Kayla Brooks on 3/23/16.
+//  Copyright (c) 2016 Robert Brooks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
