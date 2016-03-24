8917bc9ade0c3a62a5d1f96c242adced930d2f67
rebrooks6 <BrooksFamily@Kaylas-MacBook-Pro-3.local>
2016-03-24 14:40:33 -0500

    Added a zoomed view of map on second controller

---
+++ DevBootCamps/LocationVC.swift
@@ -0,0 +1,68 @@
+//
+//  SecondViewController.swift
+//  DevBootCamps
+//
+//  Created by Kayla Brooks on 3/23/16.
+//  Copyright (c) 2016 Robert Brooks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var map: MKMapView!
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        }
+        else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2, regionRadius * 2)
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        if let loc = userLocation.location {
+            centerMapOnLocation(loc)
+        }
+    }
+}
+
